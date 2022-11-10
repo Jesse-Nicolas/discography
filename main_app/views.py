69745b463ceaa4d10715acbaceef9aa5a60a1a77
Jesse Nicolas <jn@nevada.unr.edu>
2022-11-09 19:17:54 -0800

    fix nav/logo sizing

--- main_app/views.py
+++ main_app/views.py
@@ -4,70 +4,17 @@ from django.views.generic import ListView, DetailView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Album, Vibe
 from .forms import ListenForm
 
-# Create your views here. 
+# public
 class Home(LoginView):
   template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
-
-def albums_index(request):
-  albums = request.user.album_set.all()
-  return render(request, 'albums/index.html', { 'albums': albums })
-
-def albums_detail(request, album_id):
-  album = Album.objects.get(id=album_id)
-  unassigned_vibes = Vibe.objects.exclude(id__in = album.vibes.all().values_list('id'))
-  listen_form = ListenForm()
-  return render(request, 'albums/detail.html', { 'album': album, 'listen_form': listen_form, 'vibes': unassigned_vibes })
-
-class AlbumAdd(CreateView):
-  model = Album
-  fields = ['title', 'artist', 'genre', 'description', 'released']
-  def form_valid(self, form):
-    form.instance.user = self.request.user
-    return super().form_valid(form)
-
-class AlbumUpdate(UpdateView):
-  model = Album
-  fields = ['description', 'genre']
-
-class AlbumDelete(DeleteView):
-  model = Album
-  success_url = '/albums/'
-
-def add_listen(request, album_id):
-  form = ListenForm(request.POST)
-  if form.is_valid():
-    new_listen = form.save(commit=False)
-    new_listen.album_id = album_id
-    new_listen.save()
-  return redirect('albums_detail', album_id=album_id)
-
-class VibeCreate(CreateView):
-  model = Vibe
-  fields = '__all__'
-
-class VibeList(ListView):
-  model = Vibe
-
-class VibeDetail(DetailView):
-  model = Vibe
-
-class VibeUpdate(UpdateView):
-  model = Vibe
-  fields = '__all__'
-
-class VibeDelete(DeleteView):
-  model = Vibe
-  success_url = '/vibes/'
-
-def assoc_vibe(request, album_id, vibe_id):
-  Album.objects.get(id=album_id).vibes.add(vibe_id)
-  return redirect('albums_detail', album_id=album_id)
 
 def signup(request):
   error_message = ''
@@ -82,3 +29,65 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'signup.html', context)
+
+# auth protected
+@login_required
+def albums_index(request):
+  albums = request.user.album_set.all()
+  return render(request, 'albums/index.html', { 'albums': albums })
+
+@login_required
+def albums_detail(request, album_id):
+  album = Album.objects.get(id=album_id)
+  unassigned_vibes = Vibe.objects.exclude(id__in = album.vibes.all().values_list('id'))
+  listen_form = ListenForm()
+  return render(request, 'albums/detail.html', { 'album': album, 'listen_form': listen_form, 'vibes': unassigned_vibes })
+
+@login_required
+def add_listen(request, album_id):
+  form = ListenForm(request.POST)
+  if form.is_valid():
+    new_listen = form.save(commit=False)
+    new_listen.album_id = album_id
+    new_listen.save()
+  return redirect('albums_detail', album_id=album_id)
+
+@login_required
+def assoc_vibe(request, album_id, vibe_id):
+  Album.objects.get(id=album_id).vibes.add(vibe_id)
+  return redirect('albums_detail', album_id=album_id)
+
+class AlbumAdd(LoginRequiredMixin, CreateView):
+  model = Album
+  fields = ['title', 'artist', 'genre', 'description', 'released']
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class AlbumUpdate(LoginRequiredMixin, UpdateView):
+  model = Album
+  fields = ['description', 'genre']
+
+class AlbumDelete(LoginRequiredMixin, DeleteView):
+  model = Album
+  success_url = '/albums/'
+
+class VibeCreate(LoginRequiredMixin, CreateView):
+  model = Vibe
+  fields = '__all__'
+
+class VibeList(LoginRequiredMixin, ListView):
+  model = Vibe
+
+class VibeDetail(LoginRequiredMixin, DetailView):
+  model = Vibe
+
+class VibeUpdate(LoginRequiredMixin, UpdateView):
+  model = Vibe
+  fields = '__all__'
+
+class VibeDelete(LoginRequiredMixin, DeleteView):
+  model = Vibe
+  success_url = '/vibes/'
+
+
