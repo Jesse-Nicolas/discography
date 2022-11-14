1f7d04f6037c6cc4d07f0bc9f63b82865cc1fc04
Jesse Nicolas <jn@nevada.unr.edu>
2022-11-14 07:30:24 -0800

    Config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn discography.wsgi
--- discography/settings.py
+++ discography/settings.py
@@ -1,3 +1,6 @@
+import environ
+env = environ.Env()
+environ.Env.read_env()
 """
 Django settings for discography project.
 
@@ -21,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=-a5)c5i)_--kg909d@u3!#u3^95dbd1sikv9yli3-%&i(mv_v'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG') == 'True' 
 
 ALLOWED_HOSTS = []
 
@@ -133,3 +136,9 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
